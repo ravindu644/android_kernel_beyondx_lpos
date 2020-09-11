3464a59e1d6f5aa48a99f2f532941b615d228f44
Denis Efremov <efremov@linux.com>
2020-09-12 01:02:41 +0300

    kernel/sched/ems/core: check sysfs_create_file() return values
    Signed-off-by: Denis Efremov <efremov@linux.com>

--- kernel/sched/ems/core.c
+++ kernel/sched/ems/core.c
@@ -305,6 +305,8 @@ struct kobject *ems_kobj;
 static int __init init_sysfs(void)
 {
 	ems_kobj = kobject_create_and_add("ems", kernel_kobj);
+	if (!ems_kobj)
+		return -ENOMEM;
 
 	sysfs_create_file(ems_kobj, &sched_topology_attr.attr);
 	sysfs_create_file(ems_kobj, &eff_mode_attr.attr);
