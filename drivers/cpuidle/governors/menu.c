96892ffaab8dd0dd6434eff8825921cbff5f9fe1
Samuel Pascua <pascua.samuel.14@gmail.com>
2022-12-05 22:06:52 +0800

    cpuidle: menu: fix build
    Signed-off-by: Samuel Pascua <pascua.samuel.14@gmail.com>

--- drivers/cpuidle/governors/menu.c
+++ drivers/cpuidle/governors/menu.c
@@ -459,7 +459,7 @@ static int menu_select(struct cpuidle_driver *drv, struct cpuidle_device *dev,
 		}
 	}
 
-	data->last_state_idx = idx;
+	dev->last_state_idx = idx;
 
 	/* Re-evaluating the tick_stop for preserving the power and performance */
 	if (idx == 0 && data->predicted_us > TICK_USEC && *stop_tick == true) {
@@ -468,7 +468,7 @@ static int menu_select(struct cpuidle_driver *drv, struct cpuidle_device *dev,
 		*stop_tick = false;
 	}
 
-	return data->last_state_idx;
+	return dev->last_state_idx;
 }
 
 /**
