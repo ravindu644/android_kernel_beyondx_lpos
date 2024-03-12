7d332f2b1d53c63d94ef19b0ad7768278bbb9729
@ravindu644 <droidcasts@protonmail.com>
2024-03-12 10:24:29 +0530

    Redisigned : Build Script

--- build_kernel.sh
+++ build_kernel.sh
@@ -1,12 +1,29 @@
 #!/bin/bash
-clear
-export ARCH=arm64
-export PLATFORM_VERSION=12
-export ANDROID_MAJOR_VERSION=s
-ln -s /usr/bin/python2.7 $HOME/python
-export PATH=$HOME/:$HOME/toolchain/proton-clang-12/bin:$PATH
 
-ARGS="
+#Creating a symbolic link to avoid python issues.
+ln -s /usr/bin/python2.7 "$HOME/python"
+
+#exporting clang path
+export PATH="$HOME/":"$HOME/toolchain/proton-clang-12/bin":$PATH
+
+#saving current pwd as a variable
+work_dir="$(pwd)"
+
+#path for binary files
+dt_tool="$work_dir/binaries"
+repacker="$dt_tool/AIK/repackimg.sh"
+
+#setting up executable permissions
+chmod +x -R "$work_dir/binaries"
+
+#exporting variables
+export DEVICE="S10 5G"
+export KERNEL_VERSION="v8.5.1"
+
+export ARGS="
+ARCH=arm64
+PLATFORM_VERSION=12
+ANDROID_MAJOR_VERSION=s
 CC=clang
 CROSS_COMPILE=aarch64-linux-gnu-
 ARCH=arm64
@@ -22,17 +39,66 @@ LLVM_AR=llvm-ar
 LLVM_DIS=llvm-dis
 "
 
+#your defconfig
 export exynos_defconfig=exynos9820-beyondxks_defconfig
 
-work_dir=$(pwd)
-dt_tool=$work_dir/binaries
+#size in bytes for boot and recovery images.
+#beyondxks
+
+export BOOT_SIZE="57671680"
+export RECOVERY_SIZE="67633152"
+
+#cleaning output dir before building
 rm -rf out && mkdir out
 
 dtb_img() {
 	chmod +777 $dt_tool/* -R
-	$dt_tool/mkdtimg cfg_create $work_dir/out/dtb_beyondx.img $dt_tool/exynos9820.cfg -d $work_dir/arch/arm64/boot/dts/exynos
+	$dt_tool/mkdtimg cfg_create "$work_dir/out/dt.img" "$dt_tool/exynos9820.cfg" -d "$work_dir/arch/arm64/boot/dts/exynos"
 	
 	}
+
+packing(){
+    echo -e "\n\n[+] Repacking boot.img...\n\n"
+    sudo bash "$repacker"
+    echo -e "\n\n[+] Repacking Done..!\n\n"
+    mv "$dt_tool/AIK/image-new.img" "$work_dir/out/boot.img"
+
+    key(){
+    if [ ! -d "$work_dir/binaries/key" ]; then
+        mkdir "$work_dir/binaries/key"
+    fi
+    if [ ! -f "$work_dir/binaries/key/sign.pem" ]; then
+        echo -e "\n\n[+] Generating a signing key..\n\n"    
+        openssl genrsa -f4 -out "$work_dir/binaries/key/sign.pem" 4096
+    fi
+    }
+    key
+
+    sign(){
+    echo -e "\n\n[+] Signing New Boot image...\n\n" 
+    python3 "$AVBTOOL" extract_public_key --key "$work_dir/binaries/key/sign.pem" --output "$work_dir/binaries/key/sign.pub.bin"
+    python3 "$AVBTOOL" add_hash_footer --partition_name boot --partition_size "$BOOT_SIZE" --image "$work_dir/out/boot.img" --key "$work_dir/binaries/key/sign.pem" --algorithm SHA256_RSA4096
+    }
+    sign
+
+    echo -e "\n\n[+] Signing Done..!\n\n"
+    echo -e "\n\n[i] Creating a Flashable tar..!\n\n"
+
+    cd "$work_dir/out" ; tar -cvf "LPoS ${KERNEL_VERSION} [${DEVICE}] - ${SELINUX_STATUS}.tar" boot.img dt.img
+
+    echo -e "\n\n[+] Build Finished..!\n\n"
+
+}
+
+checks(){
+    if [ -f "$dt_tool/AIK/split_img/boot.img-kernel" ]; then
+        echo -e "\n\n[i] Task Finished ! \n"
+        packing 
+    else
+        echo -e "\n\n[i] Build Failed :( \n" 
+        exit 1
+    fi
+}
 
 clean_build() {
     make ${ARGS} clean && make ${ARGS} mrproper
@@ -40,8 +106,8 @@ clean_build() {
     make ${ARGS} menuconfig
     make ${ARGS} -j$(nproc)
     dtb_img
-    cp $work_dir/arch/arm64/boot/Image $work_dir/out
-    echo "Task Finished !"    
+    mv "$work_dir/arch/arm64/boot/Image" "$dt_tool/AIK/split_img/boot.img-kernel"
+    checks   
 }
 
 dirty_build() {
@@ -49,58 +115,26 @@ dirty_build() {
     make ${ARGS} menuconfig
     make ${ARGS} -j$(nproc)
     dtb_img
-    cp $work_dir/arch/arm64/boot/Image $work_dir/out
-    echo "Task Finished !"       
+    mv "$work_dir/arch/arm64/boot/Image" "$dt_tool/AIK/split_img/boot.img-kernel"
+    checks      
 }
 
-scratch() {
-    make$ {ARGS} $exynos_defconfig
-    make ${ARGS} menuconfig
-}
-
-dirty() {
-    make ${ARGS} menuconfig
-}
-
-dtb () {
-	make ${ARGS} $exynos_defconfig
-	make dtbs
-	dtb_img
-	}
-	
-clean () {
-	make ${ARGS} clean && make ${ARGS} mrproper
-	}	
-
-echo -e "Choose an Option : (1, 2, 3, 4)
-
-1. Clean build
-2. Dirty build
-3. Build Kernel config from scratch
-4. Build Kernel config from previous run
-5. Make dtb
-6. Clean the source
-
-Your choice : "
-
-read -r value
-if [ "$value" == 1 ]; then
-    clean_build
-elif [ "$value" == 2 ]; then
-    dirty_build
-elif [ "$value" == 3 ]; then
-    scratch
-elif [ "$value" == 4 ]; then
-    dirty
-elif [ "$value" == 5 ]; then
-    dtb
-elif [ "$value" == 6 ]; then
-    clean           
-else
-    echo "Invalid input"
-fi
 
 #to copy all the kernel modules (.ko) to "modules" folder.
-mkdir -p modules
-find . -type f -name "*.ko" -exec cp -n {} modules \;
-echo "Module files copied to the 'modules' folder."
+do_modules(){
+    mkdir -p modules
+    find . -type f -name "*.ko" -exec cp -n {} modules \;
+    echo "Module files copied to the 'modules' folder." 
+}
+
+USER_INPUT=$1
+
+if [ $USER_INPUT == "-c" ]; then
+    echo -e "\n\n[i] Performing a clean build...\n\n"
+    clean_build
+elif [ $USER_INPUT == "-d" ]; then
+    echo -e "\n\n[i] Performing a dirty build...\n\n"
+    dirty_build
+else
+    echo -e "\n\n[x] Wrong Input..! \n\n [i] Usage : \n\n To a Clean build : build_kernel.sh -c\n To a dirty build : build_kernel.sh -d \n"
+fi
