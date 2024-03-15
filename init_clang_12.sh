8497af6ed380bec78d40ae91730c4f0f490abb26
ghazzor <127341071+ghazzor@users.noreply.github.com>
2024-03-15 15:08:53 +0530

    speeding up workflow

--- init_clang_12.sh
+++ init_clang_12.sh
@@ -1,6 +1,2 @@
 #clone proton clang v12 toolchain
-mkdir -p toolchain
-aria2c https://cloud.ravindu-deshan.workers.dev/0:/20210123.zip
-unzip 20210123.zip
-mv proton-clang-20210123 proton-clang
-mv proton-clang toolchain
+git clone --depth=1 https://github.com/ghazzor/proton-12 toolchain
