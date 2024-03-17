87a7bd8bdb97c67ee3b5bf886f122702648fc4d2
Ravindu Deshan <Droidcasts@protonmail.com>
2024-03-17 20:23:22 +0530

    Redisigned : Build Script + Introduce : Workflow
    * Redisigned : Build Script
    
    * Introduce : Workflow
    
    From Kartik
    
    * Removed : Useless file
    
    * Adapt workflow to LPoS Kernel
    
    * Workflow : Fixes (#4)
    
    * Workflow?
    
    * Introduce : Workflow (#3)
    
    * Redisigned : Build Script
    
    * Introduce : Workflow
    
    From Kartik
    
    * Removed : Useless file
    
    * Adapt workflow to LPoS Kernel
    
    * Delete .github/workflows/main.yml
    
    * Added : Missing AVBTOOL
    
    * Speedup workflow
    
    * Useless.....!
    
    * Fixes
    
    * Added building permissive kernel support
    
    * Fixes
    
    * Refresh
    
    * Fixes
    
    * Fixes
    
    * Ramdisk : Creating missing folders before repacking
    
    * Created an another beta workflow
    
    * ChadGPT helped me
    
    * Update beta.yml
    
    * Final
    
    * Added : Compressing the final release using xz
    
    * Update beta.yml
    
    * :-|
    
    * Final fixes
    
    * Added Kernel version as Input
    
    * Exporting Kernel version
    
    * Exporting Kernel version 2
    
    * Fixes
    
    * Minor Fixes
    
    * Fixed : Permissive tar is missing
    
    * Added : patched vbmeta.img
    
    * Fixed : error
    
    * Added : Deep cleaning
    
    	modified:   build_kernel.sh

--- build_kernel.sh
+++ build_kernel.sh
@@ -1,12 +1,30 @@
 #!/bin/bash
-clear
-export ARCH=arm64
-export PLATFORM_VERSION=12
-export ANDROID_MAJOR_VERSION=s
-ln -s /usr/bin/python2.7 $HOME/python
-export PATH=$HOME/:$HOME/toolchain/proton-clang-12/bin:$PATH
 
-ARGS="
+#Creating a symbolic link to avoid python issues.
+ln -s /usr/bin/python2.7 "$HOME/python"
+
+#exporting clang path
+export PATH="$HOME/":"$HOME/toolchain/proton-clang-12/bin":$PATH
+
+#saving current pwd as a variable
+work_dir="$(pwd)"
+
+#path for binary files
+dt_tool="$work_dir/binaries"
+repacker="$dt_tool/AIK/repackimg.sh"
+AVBTOOL="$dt_tool/avbtool"
+VBMETA="$dt_tool/addons/vbmeta.img"
+
+#setting up executable permissions
+sudo chmod +775 -R "$work_dir/binaries/"
+
+#exporting variables
+export DEVICE="S10 5G"
+export KBUILD_BUILD_USER="@ravindu644"
+export ARGS="
+ARCH=arm64
+PLATFORM_VERSION=12
+ANDROID_MAJOR_VERSION=s
 CC=clang
 CROSS_COMPILE=aarch64-linux-gnu-
 ARCH=arm64
@@ -22,85 +40,145 @@ LLVM_AR=llvm-ar
 LLVM_DIS=llvm-dis
 "
 
+#your defconfig
 export exynos_defconfig=exynos9820-beyondxks_defconfig
 
-work_dir=$(pwd)
-dt_tool=$work_dir/binaries
+#size in bytes for boot and recovery images.
+#beyondxks
+
+export BOOT_SIZE="57671680"
+export RECOVERY_SIZE="67633152"
+
+#cleaning output dir before building
 rm -rf out && mkdir out
 
 dtb_img() {
-	chmod +777 $dt_tool/* -R
-	$dt_tool/mkdtimg cfg_create $work_dir/out/dtb_beyondx.img $dt_tool/exynos9820.cfg -d $work_dir/arch/arm64/boot/dts/exynos
-	
-	}
+    sudo chmod +777 $dt_tool/* -R
+    $dt_tool/mkdtimg cfg_create "$work_dir/out/dt.img" "$dt_tool/exynos9820.cfg" -d "$work_dir/arch/arm64/boot/dts/exynos"
+}
+
+packing() {
+    echo -e "\n\n[+] Repacking boot.img..."
+    cd "$dt_tool/AIK/ramdisk"
+    if [ ! -d "debug_ramdisk" ]; then
+        mkdir -p debug_ramdisk dev metadata mnt proc second_stage_resources sys
+    fi
+    cd "$work_dir"
+    sudo bash "$repacker"
+    echo -e "\n\n[+] Repacking Done..!"
+    mv "$dt_tool/AIK/image-new.img" "$work_dir/out/boot.img"
+
+    key() {
+        if [ ! -d "$work_dir/binaries/key" ]; then
+            mkdir "$work_dir/binaries/key"
+        fi
+        if [ ! -f "$work_dir/binaries/key/sign.pem" ]; then
+            echo -e "\n\n[+] Generating a signing key.."
+            openssl genrsa -f4 -out "$work_dir/binaries/key/sign.pem" 4096
+        fi
+    }
+    key
+
+    sign() {
+        echo -e "\n\n[+] Signing New Boot image..."
+        python3 "$AVBTOOL" extract_public_key --key "$work_dir/binaries/key/sign.pem" --output "$work_dir/binaries/key/sign.pub.bin"
+        sudo chmod +777 "$work_dir/out/boot.img"
+        python3 "$AVBTOOL" add_hash_footer --partition_name boot --partition_size "$BOOT_SIZE" --image "$work_dir/out/boot.img" --key "$work_dir/binaries/key/sign.pem" --algorithm SHA256_RSA4096
+    }
+    sign
+
+    echo -e "\n\n[+] Signing Done..!"
+    echo -e "\n\n[i] Creating a Flashable tar..!"
+
+    cd "$work_dir/out"
+
+    if [ ! -d "$DEVICE" ]; then
+        mkdir "${DEVICE}"
+    fi
+    if [ ! -d "${DEVICE}/${SELINUX_STATUS}" ]; then
+        mkdir "${DEVICE}/${SELINUX_STATUS}"
+    fi
+    cp "${VBMETA}" .
+    tar -cvf "LPoS ${KERNEL_VERSION} [${DEVICE}] - ${SELINUX_STATUS}.tar" boot.img dt.img vbmeta.img ; rm boot.img dt.img vbmeta.img
+    mv "LPoS ${KERNEL_VERSION} [${DEVICE}] - ${SELINUX_STATUS}.tar" "${DEVICE}/${SELINUX_STATUS}"
+}
+
+tar_xz() {
+    cd "$work_dir/out"
+    tar -cvf "LPoS [${DEVICE}].tar" ./*
+    xz -9 --threads=0 "LPoS [${DEVICE}].tar"
+    mv "LPoS [${DEVICE}].tar.xz" "LPoS [${DEVICE}].xz"
+    cd "$work_dir"
+    echo -e "\n\n[i] Compilation Done..🌛"
+}
+
+
+checks() {
+    if [ -f "$dt_tool/AIK/split_img/boot.img-kernel" ]; then
+        echo -e "\n\n[i] Task Finished ! \n"
+        packing
+    else
+        echo -e "\n\n[i] Build Failed :( \n"
+        exit 1
+    fi
+}
+
+permissive() {
+    cd "$work_dir"
+    config_file="arch/arm64/configs/$exynos_defconfig"
+
+    replace_config_option() {
+        sed -i "s/^$1=.*/$1=$2/" "$config_file"
+    }
+
+    # Modify configuration to enable SELinux permissive mode
+    replace_config_option "CONFIG_SECURITY_SELINUX_ALWAYS_PERMISSIVE" "y"
+    export SELINUX_STATUS="Permissive"
+
+    # Perform dirty build
+    dirty_build
+
+    # Revert changes back to original configuration
+    cd "$work_dir"
+    replace_config_option "CONFIG_SECURITY_SELINUX_ALWAYS_PERMISSIVE" "n"
+}
+
+deep_clean(){
+    make ${ARGS} clean && make ${ARGS} mrproper
+}
+
 
 clean_build() {
     make ${ARGS} clean && make ${ARGS} mrproper
-    make ${ARGS} $exynos_defconfig
-    make ${ARGS} menuconfig
-    make ${ARGS} -j$(nproc)
+    make ${ARGS} "$exynos_defconfig"
+    make ${ARGS} -j"$(nproc)"
     dtb_img
-    cp $work_dir/arch/arm64/boot/Image $work_dir/out
-    echo "Task Finished !"    
+    mv "$work_dir/arch/arm64/boot/Image" "$dt_tool/AIK/split_img/boot.img-kernel"
+    export SELINUX_STATUS="Enforcing"
+    checks
+    permissive
+    tar_xz
 }
 
 dirty_build() {
-    make ${ARGS} $exynos_defconfig
-    make ${ARGS} menuconfig
-    make ${ARGS} -j$(nproc)
+    make ${ARGS} "$exynos_defconfig"
+    make ${ARGS} -j"$(nproc)"
     dtb_img
-    cp $work_dir/arch/arm64/boot/Image $work_dir/out
-    echo "Task Finished !"       
+    mv "$work_dir/arch/arm64/boot/Image" "$dt_tool/AIK/split_img/boot.img-kernel"
+    checks
 }
 
-scratch() {
-    make$ {ARGS} $exynos_defconfig
-    make ${ARGS} menuconfig
-}
+USER_INPUT=$1
 
-dirty() {
-    make ${ARGS} menuconfig
-}
-
-dtb () {
-	make ${ARGS} $exynos_defconfig
-	make dtbs
-	dtb_img
-	}
-	
-clean () {
-	make ${ARGS} clean && make ${ARGS} mrproper
-	}	
-
-echo -e "Choose an Option : (1, 2, 3, 4)
-
-1. Clean build
-2. Dirty build
-3. Build Kernel config from scratch
-4. Build Kernel config from previous run
-5. Make dtb
-6. Clean the source
-
-Your choice : "
-
-read -r value
-if [ "$value" == 1 ]; then
+if [ "$USER_INPUT" == "-c" ]; then
+    echo -e "\n\n[i] Performing a clean build...\n\n"
     clean_build
-elif [ "$value" == 2 ]; then
+elif [ "$USER_INPUT" == "-d" ]; then
+    echo -e "\n\n[i] Performing a dirty build...\n\n"
     dirty_build
-elif [ "$value" == 3 ]; then
-    scratch
-elif [ "$value" == 4 ]; then
-    dirty
-elif [ "$value" == 5 ]; then
-    dtb
-elif [ "$value" == 6 ]; then
-    clean           
+elif [ "$USER_INPUT" == "-x" ]; then
+    echo -e "\n\n[i] Cleaning the source...\n\n"
+    deep_clean    
 else
-    echo "Invalid input"
+    echo -e "\n\n[x] Wrong Input..! \n\n [i] Usage : \n\n To a Clean build : build_kernel.sh -c\n To a dirty build : build_kernel.sh -d \n To Clean the source : build_kernel.sh -x\n"
 fi
-
-#to copy all the kernel modules (.ko) to "modules" folder.
-mkdir -p modules
-find . -type f -name "*.ko" -exec cp -n {} modules \;
-echo "Module files copied to the 'modules' folder."
