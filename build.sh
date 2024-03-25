7980f377c697d095e452e25df9adaa43dc44e0fb
Ravindu Deshan <Droidcasts@protonmail.com>
2024-03-25 22:31:45 +0530

    ❗️Critical Fixes [REQUIRED]
    * Revert "gpu: arm: bv_r32p1: Affine GPU IRQs to big cluster CPUs"
    
    This reverts commit 7cceb1d4b1df9ade1100dce92bbcc2994a6ff326.
    
    * Revert "gpu: arm: bv_r32p1: kbase: Reduce latency while processing ioctls"
    
    This reverts commit 07f0390ac6d7c5ee666085f201ed7591b237c41b.
    
    * Revert "drivers/video/fbdev/exynos/dpu20/decon_core: fix uninit pointer"
    
    This reverts commit 884a358f35be13dca7e37fd9d147228df45a5987.
    
    * Revert "trace: add CONFIG_DISABLE_TRACE_PRINTK option"
    
    This reverts commit a1fef51f49f7bbde259c3ae0a9ef85b8b0496ac1.
    
    * Revert "scripts: fmp: fips_fmp_integrity.py: specify readelf"
    
    This reverts commit 8744a8b56e281620b36dc1c8112caa378dc629ea.
    
    * Symlinking python2 to avoid issues

--- build.sh
+++ build.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+ln -s /usr/bin/python2.7 "$HOME/python"
 export CONFIG="exynos9820-beyondxks_defconfig"
 export PATH=$HOME/toolchain/proton-clang-12/bin:$PATH
 export LLVM=1
@@ -19,7 +20,6 @@ AS=llvm-as
 READELF=llvm-readelf
 OBJCOPY=llvm-objcopy
 OBJDUMP=llvm-objdump
-READELF=llvm-readelf
 OBJSIZE=llvm-size
 STRIP=llvm-strip
 LLVM_AR=llvm-ar
@@ -28,7 +28,7 @@ LLVM_NM=llvm-nm
 LLVM=1
 "
 
-make ${ARGS} clean && make ${ARGS} mrproper
+#make ${ARGS} clean && make ${ARGS} mrproper
 
 #patching allowlist for non-gki
 if [ ! -f ".allowlist_patched" ]; then
