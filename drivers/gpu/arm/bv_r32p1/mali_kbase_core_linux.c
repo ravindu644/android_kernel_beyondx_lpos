581ecc8b34953397a5bccb07f724249d28118fc7
@ravindu644 <droidcasts@protonmail.com>
2024-03-25 22:01:12 +0530

    Revert "gpu: arm: bv_r32p1: Affine GPU IRQs to big cluster CPUs"
    This reverts commit 7cceb1d4b1df9ade1100dce92bbcc2994a6ff326.

--- drivers/gpu/arm/bv_r32p1/mali_kbase_core_linux.c
+++ drivers/gpu/arm/bv_r32p1/mali_kbase_core_linux.c
@@ -445,7 +445,7 @@ int assign_irqs(struct kbase_device *kbdev)
 		irqtag = i;
 #endif /* CONFIG_OF */
 		kbdev->irqs[irqtag].irq = irq_res->start;
-		kbdev->irqs[irqtag].flags = (irq_res->flags & IRQF_TRIGGER_MASK) | IRQF_PERF_AFFINE;
+		kbdev->irqs[irqtag].flags = irq_res->flags & IRQF_TRIGGER_MASK;
 	}
 
 	return 0;
