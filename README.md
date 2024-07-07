76e581fb1ef50de2a5b99dc079a3fffa7e26beb2
Ravindu Deshan <Droidcasts@protonmail.com>
2024-07-07 23:33:13 +0530

    Nuke: KernelSU (#43)
    * Nuke: KernelSU
    
    * Nuke:Ksu

--- README.md
+++ README.md
@@ -1,7 +1,5 @@
 ### HOW TO BUILD
 
 ```shell
-bash build_kernel.sh -c #clean build without ksu
-
-bash build_kernel.sh -k #build with ksu , not a clean build
+bash build_kernel.sh -c #clean build
 ```
--- fs/read_write.c
+++ fs/read_write.c
@@ -25,10 +25,6 @@
 #include <linux/uaccess.h>
 #include <asm/unistd.h>
 
-#ifdef CONFIG_KSU
-#include <ksu_hook.h>
-#endif
-
 #ifdef CONFIG_SECURITY_DEFEX
 #include <linux/defex.h>
 #endif
@@ -444,10 +440,6 @@ EXPORT_SYMBOL(kernel_read);
 ssize_t vfs_read(struct file *file, char __user *buf, size_t count, loff_t *pos)
 {
 	ssize_t ret;
-
-#ifdef CONFIG_KSU
-	ksu_handle_vfs_read(&file, &buf, &count, &pos);
-#endif	
 
 	if (!(file->f_mode & FMODE_READ))
 		return -EBADF;
